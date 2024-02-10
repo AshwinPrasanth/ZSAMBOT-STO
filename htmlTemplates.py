49955d58c2944d206139f1c9579e742da4eaba90
Ashwin Prasanth <118043272+AshwinPrasanth@users.noreply.github.com>
2024-02-10 20:10:55 +0530

    Template Upload

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://e1.pngegg.com/pngimages/340/372/png-clipart-clay-os-6-a-macos-icon-adobe-ilustrator-ai-logo.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://imagedelivery.net/5MYSbk45M80qAwecrlKzdQ/3266f1fb-68a3-4c77-d23f-6b182b08b400/public">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
